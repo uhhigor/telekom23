70149cd8db58f3044042dda0ac1ed0876bdc0e7a
MaciejPerdaszek <maciej.44@wp.pl>
2023-03-31 21:37:18 +0200

    Nie wiem czemu ma to tak dzialac XD

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -1,24 +1,46 @@
 package org.example;
 
+import javax.swing.*;
 import zadanie1.singleByteErrorMessage;
 
 public class App 
 {
     public static void main( String[] args )
     {
-        short message = (short) 0xA046;
+        short[] H = new short[] {
+                (short) 0xEE8,
+                (short) 0xD94,
+                (short) 0xB42,
+                (short) 0x731,
+        };
+        short vector = (short) 0xA7;
+        System.out.println(Integer.toBinaryString(vector));
+        singleByteErrorMessage singleByteErrorMessage = new singleByteErrorMessage();
+        vector = singleByteErrorMessage.encoding(H, vector);
+        System.out.println(Integer.toBinaryString(vector));
+        vector = (short) 0xAF3;
+        System.out.println(Integer.toBinaryString(vector));
+        singleByteErrorMessage.decoding(H, vector);
+        System.out.println(Integer.toBinaryString(singleByteErrorMessage.decoding(H, vector)));
+
+
+        //System.out.println(Integer.toBinaryString(vector));
+        /*short message = (short) 0xA046;
         short[] matrix = new short[] {
                 (short) 0xF080, // 1 1 1 1 0 0 0 0 | 1 0 0 0 0 0 0 0 //1+1%2=0 /// 01000110
                 (short) 0xCC40, // 1 1 0 0 1 1 0 0 | 0 1 0 0 0 0 0 0 //1%2=1
                 (short) 0xAA20, // 1 0 1 0 1 0 1 0 | 0 0 1 0 0 0 0 0 //1+1%2=0
                 (short) 0x5610, // 0 1 0 1 0 1 1 0 | 0 0 0 1 0 0 0 0 //0
                 (short) 0xE908, // 1 1 1 0 1 0 0 1 | 0 0 0 0 1 0 0 0 //1+1=2%2=0
-                (short) 0x9504, // 1 0 0 1 0 1 0 1 | 0 0 0 0 0 1 0 0 // 1
+                (short) 0x9504, // 1 0 0 1 0 1 0 1 | 0 0 0 0 0 1 0 0 //1
                 (short) 0x7B02, // 0 1 1 1 1 0 1 1 | 0 0 0 0 0 0 1 0 //1
                 (short) 0xE701  // 1 1 1 0 0 1 1 1 | 0 0 0 0 0 0 0 1 //2%2=0
         };
         int result = singleByteErrorMessage.HTmul(matrix, message);
         System.out.println(Integer.toBinaryString(0xFFFF & message));
-        System.out.println(result);
+        System.out.println(result);*/
+
+
+
     }
 }
--- src/main/java/zadanie1/singleByteErrorMessage.java
+++ src/main/java/zadanie1/singleByteErrorMessage.java
@@ -2,7 +2,54 @@ package zadanie1;
 
 public class singleByteErrorMessage {
 
-    private final short[] H = new short[] {
+   private final short[] H = new short[] {
+           (short) 0xEE8, // 1,1,1,0,1,1,1,0, 1,0,0,0
+           (short) 0xD94, // 1,1,0,1,1,0,0,1, 0,1,0,0
+           (short) 0xB42, // 1,0,1,1,0,1,0,0, 0,0,1,0
+           (short) 0x731, // 0,1,1,1,0,0,1,1, 0,0,0,1
+   };
+
+   public short encoding(short[] matrix, short message) {
+       int sum;
+       short codeWord;
+       byte parityBits = 0;
+       for(int rows = 0; rows < 4; rows++) {
+           sum = 0;
+          for(int column = 0; column < 8; column++) {
+             sum += (matrix[rows] >> (11 - column) & 1) * (message >> (7 - column) & 1);
+           }
+          if(sum % 2 != 0) {
+              parityBits |= 0x1 << (3 - rows);
+           }
+       }
+       codeWord = (short) (message << 4 | parityBits);
+       return codeWord;
+   }
+
+    public short decoding(short[] matrix, short codeWord) {
+       short result = (short) ((codeWord >> 4) & 0xFF);
+       short errorMatrix = errorMatrix(matrix, codeWord);
+
+       return result;
+    }
+
+    public short errorMatrix(short [] matrix, short codeWord) {
+       short errorVector = 0;
+       int countSetBits = 0;
+       for (int i = 0; i < 4; i++) {
+           for(int j = 0; j < 12; j++) {
+               countSetBits += ((matrix[i] >> (11 - j) & 1)) & ((codeWord >> (11 - j) & 1));
+           }
+           int isEven = countSetBits % 2;
+           if (isEven != 0) {
+               errorVector |= 0x1 << (3 - i);
+               //System.out.println(Integer.toBinaryString(errorVector));
+           }
+       }
+       return errorVector;
+    }
+
+    /*private final short[] H = new short[] {
             (short) 0xF080, // 1 1 1 1 0 0 0 0 | 1 0 0 0 0 0 0 0
             (short) 0xCC40, // 1 1 0 0 1 1 0 0 | 0 1 0 0 0 0 0 0
             (short) 0xAA20, // 1 0 1 0 1 0 1 0 | 0 0 1 0 0 0 0 0
@@ -12,8 +59,6 @@ public class singleByteErrorMessage {
             (short) 0x7B02, // 0 1 1 1 1 0 1 1 | 0 0 0 0 0 0 1 0
             (short) 0xE701  // 1 1 1 0 0 1 1 1 | 0 0 0 0 0 0 0 1
     };
-
-
 
     private short T;
 
@@ -31,11 +76,12 @@ public class singleByteErrorMessage {
                 short sum = 0;
                 for(int c = 0; c < 16; c++) { //vector rows and matrix columns
                     sum += (matrix[matrixRow] >> (15-c) & 1) * (vector >> (15-c) & 1);
+                    //System.out.println("m " + (matrix[matrixRow] >> (15-c) & 1) + "v " + (vector >> (15-c) & 1));
                 }
-                System.out.println("Row="+matrixRow+" sum="+sum);
                 if(sum % 2 != 0)
                     result+=1;
+                System.out.println("Row="+matrixRow+" sum="+sum);
             }
         return result;
-    }
+    }*/
 }
